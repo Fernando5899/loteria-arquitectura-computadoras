a00f96bd831cdc398e31c5201ea390c7103ba0ab
Fernando5899 <f.reynosa.v023@edu.utc.mx>
2025-09-29 02:26:25 -0600

    fix: Update production CORS policy

--- server/index.ts
+++ server/index.ts
@@ -48,9 +48,10 @@ function resetGame() {
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
+
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173", "https://loteria-arquitectura-computadoras.vercel.app/"],
+        origin: ["http://localhost:5173", "https://loteria-arquitectura-computadoras.vercel.app"],
         methods: ["GET", "POST"]
     }
 });
