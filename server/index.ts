b463ef4c1fb2355b94fced3f7a7c14bb990f7bff
Fernando5899 <f.reynosa.v023@edu.utc.mx>
2025-10-06 10:10:06 -0600

    Se cambia la contraseña otra vez

--- server/index.ts
+++ server/index.ts
@@ -14,7 +14,7 @@ const ALL_WORDS = [
     'Virtualización', 'Firmware'
 ];
 const MAX_PLAYERS = 20;
-const CRIER_PASSWORD = 'Alexandra'; // Puedes cambiar esta contraseña
+const CRIER_PASSWORD = 'Fernando'; // Puedes cambiar esta contraseña
 
 type Player = { id: string; name: string; role: 'crier' | 'player' };
 
